dbecf8e6670fcc1c3069606dc7f74019a89ec4f9
hugogiesteira <hugommg18@gmail.com>
2015-12-02 14:40:01 +0000

    [update] added new getinstance

--- src/main/java/pt/uminho/ceb/biosystems/mew/core/strainoptimization/optimizationresult/solution/SolutionFactory.java
+++ src/main/java/pt/uminho/ceb/biosystems/mew/core/strainoptimization/optimizationresult/solution/SolutionFactory.java
@@ -1,6 +1,7 @@
 package pt.uminho.ceb.biosystems.mew.core.strainoptimization.optimizationresult.solution;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Map;
 
 import pt.uminho.ceb.biosystems.mew.core.simulation.components.GeneticConditions;
@@ -8,10 +9,22 @@ import pt.uminho.ceb.biosystems.mew.core.simulation.components.SteadyStateSimula
 import pt.uminho.ceb.biosystems.mew.core.strainoptimization.optimizationresult.IStrainOptimizationResult;
 
 public class SolutionFactory<T extends IStrainOptimizationResult> {
+	
+	static public <T extends IStrainOptimizationResult> T getInstance(Class<T> klazz, GeneticConditions conditions, Map<String, SteadyStateSimulationResult> results, List<Double> attributes) throws InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		T instance = klazz.getDeclaredConstructor(GeneticConditions.class, Map.class, List.class).newInstance(conditions, results, attributes);
+		return instance;
+	}
 
 	static public <T extends IStrainOptimizationResult> T getInstance(Class<T> klazz, GeneticConditions conditions, Map<String, SteadyStateSimulationResult> results) throws InstantiationException, IllegalAccessException, IllegalArgumentException,
 					InvocationTargetException, NoSuchMethodException, SecurityException {
 		T instance = klazz.getDeclaredConstructor(GeneticConditions.class, Map.class).newInstance(conditions, results);
+		return instance;
+	}
+	
+	static public <T extends IStrainOptimizationResult> T getInstance(Class<T> klazz, GeneticConditions conditions) throws InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		T instance = klazz.getDeclaredConstructor(GeneticConditions.class).newInstance(conditions);
 		return instance;
 	}
 }
